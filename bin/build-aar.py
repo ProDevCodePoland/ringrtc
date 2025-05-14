0cdeca9f0b47b4624f10478cf70f90f410ef3a3d
Mateusz Kata <prodevcodepl@gmail.com>
2025-05-14 19:05:34 +0200

    Workaround for no-camera device

--- bin/build-aar.py
+++ bin/build-aar.py
@@ -29,7 +29,7 @@ except ImportError as e:
     raise ImportError(str(e) + '- required module not found')
 
 
-DEFAULT_ARCHS = ['arm', 'arm64', 'x86', 'x64']
+DEFAULT_ARCHS = ['arm']
 NINJA_TARGETS = ['ringrtc']
 JAR_FILES = [
     'lib.java/sdk/android/libwebrtc.jar',
